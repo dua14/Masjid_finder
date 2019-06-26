6df786e73ea1a5763c724c89c75c1a4e4ebf5967
dua14 <dbatool91@gmail.com>
2019-06-26 22:03:28 +0500

    first commit

---
+++ Masjid/src/masjid/Masjid.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package masjid;
+
+/**
+ *
+ * @author Dua Batool
+ */
+public class Masjid {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//Mosqueid obj = new Mosqueid();
+   // SelectMosque obj=new SelectMosque();
+    First obj=new First();  
+   obj.setVisible(true);
+   obj.setSize(430, 457);
+     obj.setResizable(false);
+    }
+}
